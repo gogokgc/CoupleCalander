7b3c3ae79d9129cc68524769d1e9ac1c1f6e6814
gogokgc <gogokgc@gmail.com>
2024-05-15 12:17:31 +0900

    Initial Commit

---
+++ CoupleCalander/CoupleCalanderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoupleCalanderApp.swift
+//  CoupleCalander
+//
+//  Created by KYUCHEOL KIM on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CoupleCalanderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
